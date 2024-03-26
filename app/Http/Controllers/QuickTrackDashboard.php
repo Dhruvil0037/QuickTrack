ffa0c8ea6213d15599510f6662bea5c75942ce85
dhruvilp02 <dhruvilpatel.myappgurus@gmail.com>
2024-03-26 17:22:32 +0530

    laravel backend first

--- app/Http/Controllers/QuickTrackDashboard.php
+++ app/Http/Controllers/QuickTrackDashboard.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class QuickTrackDashboard extends Controller
 {
@@ -10,6 +11,71 @@ class QuickTrackDashboard extends Controller
     {
         $data['data'] = $request->data;
         $data['appName'] = $request->appName;
+
+        $parts = explode('-', $data['data'], 2);
+        
+        if (isset($parts[0]) && isset($parts[1])) {
+            $data['merchantData'] = DB::table('user_additional_datas')->where('store_url', $parts[1])->first();
+            // $data['merchantData'] = DB::table('user_additional_datas')->where('store_url', 'quickstart-e69a1402.myshopify.com')->first();
+
+            $data['merchantUserData'] = [];
+
+            if(isset($data['merchantData'])){
+                $data['merchantUserData'] = DB::table('product_reviews')->where('user_id', $data['merchantData']->user_id)->get();
+            }
+            //dd($data['userData']);
+            
+        } else {
+            switch ($data['data']) {
+                case 'AllData':
+                    // May be store_is_inactive can change to 1
+                    $data['activeMerchantCounts'] = DB::table('users')->where('store_is_inactive', 0)->count();
+
+                    $data['allReviewCounts'] = DB::table('product_reviews')->count();
+
+                    $data['freeMerchantCounts'] = DB::table('users')->count();
+
+                    $data['paidMerchantCounts'] = DB::table('users')->count();
+
+                    $data['topTenActiveMerchants'] = DB::table('users')
+                                ->selectRaw('*, DATE_FORMAT(created_at, "%d-%m-%Y") as formatted_created_at')
+                                ->where('store_is_inactive', 0)
+                                ->orderBy('created_at', 'desc')
+                                ->get();
+
+                    $data['topTenLoginMerchants'] = DB::table('users')
+                                ->selectRaw('*, DATE_FORMAT(created_at, "%d-%m-%Y") as formatted_created_at')
+                                ->where('store_is_inactive', 0)
+                                ->orderBy('created_at', 'desc')
+                                ->get();
+                    break;
+                case 'ActiveMerchantList':
+                    $data['activeMerchants'] = DB::table('users')
+                                ->selectRaw('*, DATE_FORMAT(created_at, "%d-%m-%Y") as formatted_created_at')
+                                ->where('store_is_inactive', 0)
+                                ->orderBy('created_at', 'desc')->get();
+                    break;
+                case 'AllMerchantList':
+                    $data['allMerchants'] = DB::table('users')
+                                ->selectRaw('*, DATE_FORMAT(created_at, "%d-%m-%Y") as formatted_created_at')
+                                ->orderBy('created_at', 'desc')->get();
+                    break;
+                case 'FreeMerchantList':
+                    $data['freeMerchants'] = DB::table('users')
+                                ->selectRaw('*, DATE_FORMAT(created_at, "%d-%m-%Y") as formatted_created_at')
+                                ->orderBy('created_at', 'desc')->get();
+                    break;
+                case 'PaidMerchantList':
+                    $data['paidMerchants'] = DB::table('users')
+                                ->selectRaw('*, DATE_FORMAT(created_at, "%d-%m-%Y") as formatted_created_at')
+                                ->orderBy('created_at', 'desc')->get();
+                    break;
+                default:
+                    // default case
+                    break;
+                    
+            }
+        }
         
         return response()->json($data);
     }
